5668fac9725c50e31f065d6af7d0ca9301d0ce05
sanxlop <sanchez.11.93@gmail.com>
2015-04-29 18:39:07 +0200

    creditos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var author = require('./routes/author');
 
 var app = express();
 
@@ -20,8 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/images')));
 
 app.use('/', routes);
+app.use('/author', author);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
