5d0b0009df610af1ec389597f454d213489d37cf
sanxlop <sanchez.11.93@gmail.com>
2015-04-29 18:10:39 +0200

    Prueba
    This reverts commit 1230e385650f22d7795597946941c839eedbe38c.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var author = require('./routes/author');
 
 var app = express();
 
@@ -16,7 +15,6 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(foto_autores(__dirname + '/public/foto_autores.jpg'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/author', author);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
