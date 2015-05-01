973f391e7d82966967b90aba7490065056a74c5e
sanxlop <sanchez.11.93@gmail.com>
2015-05-01 20:47:58 +0200

    quinto

--- models/models.js
+++ models/models.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+ 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
@@ -7,21 +7,21 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize(null, null, null, 
   { dialect:  "sqlite", storage: "quiz.sqlite"}      
 );
-
+ 
 // Importar definicion de la tabla Quiz
-var Quiz = sequelize.import(quiz_path);
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz; // exportar tabla Quiz
 
 // sequelize.sync() inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
+  Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.bulkCreate( 
-         {pregunta: 'Capital de Italia',   respuesta: 'Roma'
-       })
-      .success(function(){console.log('Base de datos inicializada')});
+      Quiz.create({ pregunta: 'Capital de Portugal',   
+                    respuesta: 'Lisboa'
+      })
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
