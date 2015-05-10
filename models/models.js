80f39010a56194ef9a393b5c58fc40064263e310
sanxlop <sanchez.11.93@gmail.com>
2015-05-10 17:55:06 +0200

    lista de preguntas

--- models/models.js
+++ models/models.js
@@ -37,6 +37,9 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',   
+                    respuesta: 'Roma'
+      });
       Quiz.create({ pregunta: 'Capital de Portugal',   
                     respuesta: 'Lisboa'
       })
