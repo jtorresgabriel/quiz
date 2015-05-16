fc3fa6a504dd76e372337c01547795afd852f1f3
sanxlop <sanchez.11.93@gmail.com>
2015-05-16 18:45:21 +0200

    añadir comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -20,15 +21,14 @@ router.get('/author', function(req, res) {
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
-
 router.get('/quizes/search', quizController.search);
-
 router.get('/quizes/:quizId(\\d)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d)', quizController.update);
-
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
