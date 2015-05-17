9e18107b9443445817e48eb43bb64d06744f1cc8
sanxlop <sanchez.11.93@gmail.com>
2015-05-17 18:58:12 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 /* Autoload de comandos con :quizId */
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 /* GET author page. */
 router.get('/author', function(req, res) {
@@ -37,5 +38,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 /* Definicion de rutas de comentarios */
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
