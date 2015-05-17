32bddec5fd200ff67ed8dac6ebea8e8f09d50bcb
sanxlop <sanchez.11.93@gmail.com>
2015-05-17 13:19:57 +0200

    autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,6 +18,11 @@ router.get('/author', function(req, res) {
   res.render('author',{ errors: [] });
 });
 
+/* Definicion de rutas de sesion */
+router.get('/login', sessionController.new); //formulario login
+router.post('/login', sessionController.create); //crear sesion
+router.get('/logout', sessionController.destroy); //destruir sesion
+
 /* Definicion de rutas de /quizes */
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -28,6 +34,7 @@ router.get('/quizes/:quizId(\\d)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+/* Definicion de rutas de comentarios */
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
